f2057d6124f6846b59371d665f733e900a8959e5
SIEENA\arturo.rodrigueza <arturo.rodrigueza@definityfirst.com>
2019-02-01 12:04:58 -0600

    Simplified merge_sort

--- merge_sort/merge_sort.py
+++ merge_sort/merge_sort.py
@@ -1,6 +1,6 @@
 import math
 
-def merge(a1, a2):
+def merge(a1, a2, decreasing):
     i = 0
     j = 0
     a = []
@@ -15,31 +15,7 @@ def merge(a1, a2):
             i += 1
             continue
 
-        if (a1[i] <= a2[j]):
-            a.append(a1[i])
-            i += 1
-        else:
-            a.append(a2[j])
-            j += 1
-    
-    return a
-
-def merge_decreasing(a1, a2):
-    i = 0
-    j = 0
-    a = []
-
-    while i < len(a1) or j < len(a2):
-        if i >= len(a1):
-            a.append(a2[j])
-            j += 1
-            continue
-        elif j >= len(a2):
-            a.append(a1[i])
-            i += 1
-            continue
-
-        if (a1[i] >= a2[j]):
+        if (a1[i] >= a2[j] if decreasing else a1[i] <= a2[j]):
             a.append(a1[i])
             i += 1
         else:
@@ -59,8 +35,8 @@ def merge_sort_recursive(array, startIndex, endIndex, decreasing = False):
     
     a1 = merge_sort_recursive(array, startIndex, middleIndex, decreasing)
     a2 = merge_sort_recursive(array, middleIndex + 1, endIndex, decreasing)
-    
-    return merge_decreasing(a1, a2) if decreasing else merge(a1, a2)
+
+    return merge(a1, a2, decreasing)
 
 def merge_sort(array):
     return merge_sort_recursive(array, 0, len(array))
